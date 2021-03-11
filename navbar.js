e769971bc8b630c7f30ea22635ce76a8c53edeb9
Josh Little <jlittle10@cnm.edu>
2021-03-11 09:27:44 -0700

    updated hero section with javascript

---
+++ navbar.js
@@ -0,0 +1,24 @@
+
+const slideImg = document.getElementById("slideImg");
+
+const images = new Array (
+         "images/photo1.jpg",
+          "images/photobanner2.jpg",
+          "images/photobanner3.jpg",
+          "images/photonanner4.jpg"
+
+);
+
+const len = images.length;
+let i = 0;
+
+function slider(){
+    if (i > len-1) {
+    i = 0;
+}
+    slideImg.src = images[i];
+     i ++;
+    setTimeout('slider()', 2000);
+
+}
+
